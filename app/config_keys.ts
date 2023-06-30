70c087634df05b8876296ce7bcf2e36985347451
Terraform05 <60861553+Terraform05@users.noreply.github.com>
2023-06-30 10:35:40 -0500

    Delete config_keys.ts

--- app/config_keys.ts
+++
@@ -1,3 +0,0 @@
-export const client_id = '0e19ab44ed9945b889a32dd22db8b86b';
-export const client_secret = '30024828c2c64513b9dd685bae57dd9a';
-export const redirect_uri = 'http://localhost:3000/callback';
